8c5b916e1361f42945f1ac7a784c685e24fe81ca
Nath <playerxmatt@gmail.com>
2024-03-05 11:10:34 -0300

    feat: added search input to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { createSignal } from "solid-js";
+
 function App() {
+  const [searchTerm, setSearchTerm] = createSignal("");
+
   return (
-    <div>
-      <input></input>
+    <div class="flex h-screen justify-center bg-neutral-100 p-2 dark:bg-neutral-900">
+      <input
+        class="h-fit w-full rounded-lg px-5 py-2 leading-normal text-neutral-950 shadow shadow-neutral-300"
+        value={searchTerm()}
+        onChange={(e) => setSearchTerm(e.currentTarget.value)}
+      />
     </div>
   );
 }
