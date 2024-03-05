df63026f5f19d221262236162ca4e0a3b0a074d6
Nath <playerxmatt@gmail.com>
2024-03-05 15:05:10 -0300

    feat: added App structure to main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use applications;
+use std::path::PathBuf;
+
+pub struct App {
+    pub name: String,
+    pub icon_path: Option<PathBuf>,
+    pub app_path_exe: PathBuf,
+    pub app_desktop_path: PathBuf,
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
