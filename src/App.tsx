44a8765e2e7349cff3819df700628629fddefc85
Nath <playerxmatt@gmail.com>
2024-03-05 18:41:26 -0300

    feat: app list is filtered with search term

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { invoke } from "@tauri-apps/api";
-import { For, createSignal, onMount } from "solid-js";
+import { For, createEffect, createSignal, onMount } from "solid-js";
 
 interface AppType {
   name: string;
@@ -11,6 +11,7 @@ interface AppType {
 function App() {
   const [searchTerm, setSearchTerm] = createSignal("");
   const [apps, setApps] = createSignal<AppType[]>();
+  const [shownApps, setShownApps] = createSignal<AppType[]>();
 
   onMount(() => {
     async function get_apps() {
@@ -20,16 +21,39 @@ function App() {
     get_apps();
   });
 
+  createEffect(() => {
+    if (searchTerm() === "") {
+      setShownApps([]);
+      return;
+    }
+
+    setShownApps(
+      apps()
+        ?.filter((app) => {
+          return app.name.match(new RegExp(searchTerm(), "gi"));
+        })
+        .sort((a, b) => {
+          if (a.name < b.name) {
+            return -1;
+          }
+          if (a.name > b.name) {
+            return 1;
+          }
+          return 0;
+        }),
+    );
+  });
+
   return (
-    <div class="flex h-screen justify-center bg-neutral-100 p-2 dark:bg-neutral-900">
+    <div class="flex flex-col justify-center bg-neutral-100 p-2 dark:bg-neutral-900">
       <input
         class="h-fit w-full rounded-lg px-5 py-4 text-lg text-neutral-950 shadow shadow-neutral-300 dark:bg-neutral-800 dark:text-neutral-100 dark:shadow-neutral-950"
         placeholder="BigBrainSearch"
         value={searchTerm()}
-        onChange={(e) => setSearchTerm(e.currentTarget.value)}
+        onKeyUp={(e) => setSearchTerm(e.currentTarget.value)}
       />
       <div class="flex flex-col overflow-y-auto">
-        <For each={apps()}>{(app) => <p>{app.name}</p>}</For>
+        <For each={shownApps()}>{(app) => <p>{app.name}</p>}</For>
       </div>
     </div>
   );
