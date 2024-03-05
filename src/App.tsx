9149370361775ab36b00e6c0cd40c7dbe8d84e6b
Nath <playerxmatt@gmail.com>
2024-03-05 11:29:56 -0300

    feat: added darkmode to input

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ function App() {
   return (
     <div class="flex h-screen justify-center bg-neutral-100 p-2 dark:bg-neutral-900">
       <input
-        class="h-fit w-full rounded-lg px-5 py-2 leading-normal text-neutral-950 shadow shadow-neutral-300"
+        class="h-fit w-full rounded-lg px-5 py-2 leading-normal text-neutral-950 shadow shadow-neutral-300 dark:bg-neutral-800 dark:text-neutral-100 dark:shadow-neutral-950"
+        placeholder="BigBrainSearch"
         value={searchTerm()}
         onChange={(e) => setSearchTerm(e.currentTarget.value)}
       />
