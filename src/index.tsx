55144b5578c7e92293684d084d3fe26eec481cfe
Nath <playerxmatt@gmail.com>
2024-03-05 23:09:01 -0300

    feat: make window center on startup

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,8 @@ import { render } from "solid-js/web";
 import "./styles.css";
 import App from "./App";
 
+import { LogicalSize, appWindow } from "@tauri-apps/api/window";
+await appWindow.setSize(new LogicalSize(600, 30));
+await appWindow.center();
+
 render(() => <App />, document.getElementById("root") as HTMLElement);
