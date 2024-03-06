eef590738cbae7b6842f0794821067ca76cea2dc
Nath <playerxmatt@gmail.com>
2024-03-05 23:12:26 -0300

    feat: made ResultList h-full

--- src/components/ResultList.tsx
+++ src/components/ResultList.tsx
@@ -8,7 +8,7 @@ export default function ResultList({
 }) {
   return (
     <Show when={results().length > 0}>
-      <ul class="flex flex-col gap-2 overflow-y-auto rounded-lg bg-white px-5 py-4 text-neutral-950 shadow shadow-neutral-300 dark:bg-neutral-800 dark:text-neutral-100 dark:shadow-neutral-950">
+      <ul class="flex h-full flex-col gap-2 overflow-y-auto rounded-lg bg-white px-5 py-4 text-neutral-950 shadow shadow-neutral-300 transition dark:bg-neutral-800 dark:text-neutral-100 dark:shadow-neutral-950">
         <For each={results()}>
           {(result) => <Application appData={result} />}
         </For>
